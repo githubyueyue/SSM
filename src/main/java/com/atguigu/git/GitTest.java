a1c2e0ac15618b17d32f15ad59775ab21c37ebf3
Layne <Layne@qq.com>
2022-08-05 17:01:02 +0800

    second commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @author fl
+ * @create 2022-08-05 16:44
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+        System.out.println("hello,git2!");
+    }
+
+}
